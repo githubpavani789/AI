022a4fc4b6f5ab0cef65b473beec797b15837833
githubpavani789 <90465808+githubpavani789@users.noreply.github.com>
2026-03-10 17:21:13 +0530

    Update ExceptionController.java

--- src/main/java/com/example/ai_workflow_demo/ExceptionController.java
+++ src/main/java/com/example/ai_workflow_demo/ExceptionController.java
@@ -14,6 +14,6 @@ public class ExceptionController {
         // This will throw a real NullPointerException
         int length = str .lengtch();
 
-        return "Length is " + length;
+        return "Length is " + lecngth;
     }
 }
