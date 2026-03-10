67067ca8df4cf492c05c95c2c4ba75c84215d271
Pavani Kalidindi <Pavani.Kalidindi@entaingroup.com>
2026-03-10 12:56:12 +0530

    Add ExceptionController with null-pointer endpoint

---
+++ src/main/java/com/example/ai_workflow_demo/ExceptionController.java
@@ -0,0 +1,18 @@
+package com.example.ai_workflow_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExceptionController {
+
+    @GetMapping("/null-pointer")
+    public String triggerNullPointer() {
+        String str = null;
+
+        // This will throw a real NullPointerException
+        int length = str.length();
+
+        return "Length is " + length;
+    }
+}
