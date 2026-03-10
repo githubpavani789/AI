bc5bda69eba0ee017db06db7fc6a54038c2734f6
Pavani Kalidindi <Pavani.Kalidindi@entaingroup.com>
2026-03-10 10:04:22 +0530

    Initial commit: Jenkins deployment demo

---
+++ src/main/java/com/example/ai_workflow_demo/AiWorkflowDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_workflow_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiWorkflowDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiWorkflowDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ai_workflow_demo/AiWorkflowDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_workflow_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWorkflowDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
